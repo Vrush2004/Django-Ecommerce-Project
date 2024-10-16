9e6dbf903c7915ee97954bf2b006653c8c45cbf9
Vrush2004 <vrushalichaube@gmail.com>
2024-10-16 18:06:29 +0530

    changes

--- Django_ecommerce_project/ec/app/urls.py
+++ Django_ecommerce_project/ec/app/urls.py
@@ -11,9 +11,13 @@ urlpatterns = [
     path("contact/", views.contact, name="contact"),
     path("category/<slug:val>", views.CategoryView.as_view(),name="category"),
     path("category-title/<val>", views.CategoryTitle.as_view(),name="category-title"),
+    path("productdetail/<int:pk>/", views.ProductDetail.as_view(), name="productdetail"),
     path("profile/",views.ProfileView.as_view(),name='profile'),
     path("address/",views.address,name='address'),
     path("updateAddress/<int:pk>",views.updateAddress.as_view(),name="updateAddress"),
+
+    path("add-to-cart/", views.add_to_cart, name= "add_to_cart"),
+    path("cart/", views.show_cart, name='showcart'),
 
     #login authentication
     path("registration/", views.CutomerRegistrationView.as_view(),name="customerregistration"),
